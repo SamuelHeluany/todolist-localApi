2c4aa79e5b2e1d19552c0ee8a5d66fabf9803078
Samuel Heluany <samuelheluany@hotmail.com>
2026-05-16 16:24:48 -0300

    create css todolist

--- todolist-frontend/src/Components/TodoList/TodoList.tsx
+++ todolist-frontend/src/Components/TodoList/TodoList.tsx
@@ -1,20 +1,34 @@
 import './TodoList.css'
+import { MdEdit, MdDelete  } from "react-icons/md";
+import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
+
+interface Todo {
+    id: string,
+    title: string,
+    status: string
+}
+
+interface ApiResponse<T> {
+    content: T[],
+    totalElements: number,
+    message: string
+}
 
 const todoList = () => {
     return (
         <div className="container">
             <div className="todoList">
                 <div className="headerTodo">
-                    <h1>TodoList</h1>
-                    <button>Create Todo</button>
+                    <h1>Todo-List</h1>
+                    <button className='todoCreateButton'>Create Todo</button>
                 </div>
                 <div className="todos">
                         <ul>
-                            <li>teste 
+                            <li><p>teste</p> 
                                 <div className="actions">
-                                    <button>edit</button>
-                                    <button>delete</button>
-                                    <button>Done</button>
+                                    <button><MdEdit size={25} color='rgb(2, 61, 112)'/></button>
+                                    <button><MdDelete size={25} color='red'/></button>
+                                    <button><IoCheckmarkDoneCircleSharp size={25} color='green'/></button>
                                     </div></li>
                         </ul>
                     </div>
